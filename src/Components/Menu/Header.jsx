6fffc89344bc284c4341f369eb236cde46314ae7
depst0r <depstor90@gmail.com>
2020-11-26 11:59:49 +0300

    'start'

---
+++ src/Components/Menu/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Header = () => {
+    return <>
+    <header>
+        <ul>
+            <li>Главная</li>
+            <li>Новости</li>
+            <li>Вход/Выход</li>
+        </ul>
+    </header>
+    </>
+}
