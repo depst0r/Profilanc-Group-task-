513ca9044cd6da7a2a32d66a15a230aca5d19fd5
depst0r <depstor90@gmail.com>
2020-11-29 12:13:49 +0300

    'date'

--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export const HomePage = () => {
-
+console.log(new Date().toLocaleDateString());
     return <>
         <h1>Привет</h1>
     </>
--- src/Redux/Reducers/reducer.js
+++ src/Redux/Reducers/reducer.js
@@ -1,6 +1,7 @@
 import { ADD_NEWS, DELETE_NEWS } from '../Actions/actionTypes'
 
-const initialNews = [
+
+const initialState = [
     {
         title: 'NEWS',
         date: '25.11.2020',
@@ -19,25 +20,20 @@ const initialNews = [
         text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
         completed: false
     },
-
 ]
-
-const initialState = {
-        news: '',
-        id: '',
-        
-
-}
 
 export default function reducer(state = initialState, action) {
     switch (action.type) {
         case ADD_NEWS:
-                console.log('ADD', action.news)
-            return {
+            return [
                 ...state,
-                news: action.news,
-                id: action.id,
-            }
+                {
+                    title: action.title,
+                    date: new Date().toLocaleDateString(),
+                    text: action.text,
+                    completed: false
+                }
+            ]
         default:
             return state
     }
