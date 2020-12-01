3afe50e96de7a8d306b6ffcd066a8cc99ea5f6af
depst0r <depstor90@gmail.com>
2020-12-01 17:13:59 +0300

    'add-start'

--- src/Components/News/CreateNews.jsx
+++ src/Components/News/CreateNews.jsx
@@ -1,19 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { addNews } from '../../Redux/Actions/actions'
 
 export const CreateNews = () => {
+
+const [title, setTitle] = useState()
+const [text, setText] = useState()
 
     return <>
         <div>
             <div className="row m-2">
-                <input type="Text"
+                <input 
+                type="Text"
                 className="col form-control" 
                 placeholder="title"
+                onSubmit={e => setTitle(e.target.value)}
+                value={title}
                 />
-                <input type="text"
+                <input 
+                type="text"
                 className="col form-control" 
                 placeholder="Text"
+                onSubmit={e => setText(e.target.value)}
+                value={text}
                 />
                 <button 
+                onClick={() => addNews(
+                    {
+                        id: Date.now(),
+                        date: new Date().toLocaleDateString(),
+                        title: title,
+                        text: text
+                    }
+                )}
                 className="col btn btn-primary mx-2">
                     Post News
                 </button>
